0232b0bfa3fb46b232e6a363f588562b23c62068
anshgaba23304 <122441969+anshgaba23304@users.noreply.github.com>
2023-03-23 12:25:24 +0530

    Tkinter buttons
    Defined some basic functions and Tkinter buttons

--- Scientific_Calculator.py
+++ Scientific_Calculator.py
@@ -98,4 +98,50 @@ def third_root():
     else:
         temp = "ERROR"
     text_input.set(temp)
+    def sign_change():
+    global calc_operator
+    if calc_operator[0]=='-':
+        temp = calc_operator[1:]
+    else:
+        temp = '-'+calc_operator
+    calc_operator = temp
+    text_input.set(temp)    
+
+
+def percent():
+    global calc_operator
+    temp = str(eval(calc_operator+'/100'))
+    calc_operator = temp
+    text_input.set(temp)
+
+
+def button_equal():
+    global calc_operator
+    temp_op = str(eval(calc_operator))
+    text_input.set(temp_op)
+    calc_operator = temp_op 
+
+sin, cos, tan = math.sin, math.cos, math.tan
+log, ln = math.log10, math.log
+e = math.exp
+p = math.pi
+E = '*10**'
+try:
+    print("0/0")
+except:
+    print("ERROR")
+
+
+tk_calc = Tk()
+tk_calc.configure(bg="#293C4A", bd=10)
+tk_calc.title("Scientific Calculator")
+
+calc_operator = ""
+text_input = StringVar()
+
+text_display = Entry(tk_calc, font=('sans-serif', 20, 'bold'), textvariable=text_input,
+                     bd=5, insertwidth = 5, bg='#BBB', justify='right').grid(columnspan=5, padx = 10, pady = 15)
+
+button_params = {'bd':5, 'fg':'#BBB', 'bg':'#3C3636', 'font':('sans-serif', 20, 'bold')}
+button_params_main = {'bd':5, 'fg':'#000', 'bg':'#BBB', 'font':('sans-serif', 20, 'bold')}
 
